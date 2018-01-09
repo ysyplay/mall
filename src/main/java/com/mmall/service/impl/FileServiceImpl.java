64576879b375085d2a69147a49be73c30a96e1d6
ysyplay <1094510449@qq.com>
2018-01-09 23:35:31 +0800

    readme

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -36,9 +36,9 @@ public class FileServiceImpl implements IFileService {
         try {
             file.transferTo(targetFile);
             //文件已经上传成功了
-//           FTPUtil.uploadFile(Lists.newArrayList(targetFile));
+           FTPUtil.uploadFile(Lists.newArrayList(targetFile));
 //            //已经上传到ftp服务器上
-//            targetFile.delete();
+            targetFile.delete();
         } catch (IOException e) {
             logger.error("上传文件异常",e);
             return null;
